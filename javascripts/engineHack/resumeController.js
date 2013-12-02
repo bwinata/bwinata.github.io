fd12e153553cea5737ee868b688c8a882312dead
Barry Winata <barry.winata@yahoo.com>
2013-12-02 23:44:33 +1100

    Updated CV

--- javascripts/engineHack/resumeController.js
+++ javascripts/engineHack/resumeController.js
@@ -1,11 +1,2 @@
 
 var resumeApp = angular.module ('resumeApp', []);
-
-resumeApp.controller ('resume', function ($scope) {
-	$scope.education = [
-		{'period' : '2006 - 2011 : Alma Mater'},
-		{'name' : 'University of Technology, Sydney (Dean\'s List)'},
-		{'name' : 'Bachelor of Electrical / Electronic Engineering (Hons, First Class)'},
-		{'name' : 'Bachelor of Business (Fin) [Distinction]'}
-	];
-});
